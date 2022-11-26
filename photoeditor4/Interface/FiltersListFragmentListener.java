ed301f9ca947ca0a62771cff661395421f9c9b28
Josue Flores <Flores.Josue427@gmail.com>
2022-11-26 15:54:23 -0500

    Updated the files to the old, functional version

--- photoeditor4/Interface/FiltersListFragmentListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.photoeditor4.Interface;
-
-import com.zomato.photofilters.imageprocessors.Filter;
-
-public interface FiltersListFragmentListener {
-    void onFilterSelected(Filter filter);
-}
